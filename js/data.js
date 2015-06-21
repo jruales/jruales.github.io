8e043ba0793d280051a4b720cd73ee1a28a388df
Joaquín Ruales <joaqo182@gmail.com>
2015-06-21 14:56:23 -0700

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,84 @@
+prePrints = [
+  {
+    title:"On left-orderability and double branched covers of Kanenobu's knots",
+    authors:"Fabian Doria Medina, Michael Jackson, Joaquín Ruales, Hadas Zeilberger",
+    abstract:"We show that the fundamental group of the double branched cover of an infinite family of homologically thin, non-quasi-alternating knots is not left-orderable, giving further support for a conjecture of Boyer, Gordon, and Watson that an irreducible rational homology 3-sphere is an L-space if and only if its fundamental group is not left-orderable."
+    journal:"ArXiv",
+    website:"http://arxiv.org/abs/1310.1877"
+  }
+];
+
+work: [
+  {
+    company: "Google",
+    position: "Software Engineering Intern",
+    website: "https://www.google.com/",
+    startDate: "2015-06-01"
+    endData: "2015-09-04"
+  },
+  {
+    company: "Google",
+    position: "Software Engineering Intern",
+    website: "https://www.google.com/",
+    startDate: "2014-06"
+    endData: "2014-08"
+  },
+  {
+    company: "Columbia University in the City of New York",
+    position: "Research Assistant",
+    website: "http://www.columbia.edu/",
+    startDate: "2014-06"
+    endData: "2014-08"
+  },
+  {
+    company: "Columbia University in the City of New York",
+    position: "Library Website Developer/Designer",
+    website: "http://www.columbia.edu/",
+    startDate: "2014-06"
+    endData: "2014-08"
+  }
+]
+
+websites = [
+{
+
+}
+]
+
+
+
+/*
+{
+  "basics": {
+    "name": "Joaquín Ruales",
+    "label": "Student at Columbia University",
+    "picture": "https://media.licdn.com/mpr/mprx/0_vJv4JCCofLjlSRVCzMNIJhtwDhRASYVCBgTbJ3A9-Qf3tVDGJ05qBT54iDUaaMRmNVB69Q73CxcG",
+    "email": "joaqo182@gmail.com",
+    "phone": "",
+    "website": "",
+    "location": {
+      "address": "",
+      "postalCode": "",
+      "city": "Greater New York City Area",
+      "countryCode": "US",
+      "region": ""
+    },
+    "profiles": []
+  },
+  "work": [
+    {
+      "company": "Google",
+      "position": "Software Engineering Intern",
+      "website": "",
+      "startDate": "2015-06-01",
+      "highlights": []
+    }
+  ],
+  "education": [],
+  "skills": [],
+  "languages": [],
+  "references": [],
+  "volunteer": [],
+  "awards": []
+}
+*/
