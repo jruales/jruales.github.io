20fc3bcde05a44bb1db3f58a8037b923dadc175d
Joaquín Ruales <joaqo182@gmail.com>
2015-07-19 17:32:32 -0700

    removed minimap due to smartphone crashing

--- js/main.js
+++ js/main.js
@@ -39,7 +39,6 @@ $(function() {
   var template = Handlebars.compile(source);
   $("#main").html(template(context));
 
-  var previewBody = $('#main').minimap();
   //$( "#accordion" ).accordion();
 });
 
